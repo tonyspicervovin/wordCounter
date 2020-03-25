3a02b09ef63cdbf0b8702af3c71d9a02eeb75e6f
tonyspicervovin <42811702+tonyspicervovin@users.noreply.github.com>
2020-03-25 16:01:48 -0500

    Finding max word and other words with same occurances

--- wordCount.py
+++ wordCount.py
@@ -1,6 +1,6 @@
 def main():
     #initial array of words
-    arr = ["the", "floor", "plan", "has", "a", "plan", "for", "everyone", "in", "the", "floor"]
+    arr = ["the", "floor", "plan", "has", "a", "plan", "for", "everyone", "in", "the", "floor", "floor", "the"]
     #sorting array
     arr.sort()
     #initializing a dictionary for words and their occurance
@@ -13,9 +13,24 @@ def main():
             word_counter[word] += 1
         else:
             word_counter[word] = 1
-    
-    popular_words = sorted(word_counter, key = word_counter.get, reverse = True)
-    print(word_counter)
+    #creating a list to hold all max words
+    all_max_words= []
+    #finding the maximum amount of times a word has occured
+    maxOccurance = max(word_counter, key = word_counter.get)
+    #finding the word with the most occurances, although this will only find me one word
+    #and there may be more words that share the same amount of occurances
+    maxWord = (max(word_counter, key=word_counter.get))
+    #adding the most occuring word in to list
+    all_max_words.append(maxWord)
+    #finding how many times it occured
+    maxOccurance = word_counter.get(maxWord)
+   #looping through dictionary and finding other words with occurances equal to the max word
+   #this is finding words that have occured the same amount of times as our "max" and adding
+   #them to the all max word list as long as the word isn't the original one
+    for word in word_counter:
+        if word_counter[word] == maxOccurance and word != maxWord:
+            all_max_words.append(word)
+    print(all_max_words)
     
     
     
